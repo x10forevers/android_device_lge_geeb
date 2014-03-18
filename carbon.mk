f2d7227b89393d521fc9487d7ecad16ff825ef09
mathsz <mathieuvalotaire@hotmail.com>
2014-03-17 20:44:13 -0400

    carbon specifics

---
+++ carbon.mk
@@ -0,0 +1,28 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Enhanced NFC
+# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/geeb/full_geeb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := geeb
+PRODUCT_NAME := carbon_geeb
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-GEEB
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/occam/GEEB_CA:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="ERRMYGERDITWOKS"
+
